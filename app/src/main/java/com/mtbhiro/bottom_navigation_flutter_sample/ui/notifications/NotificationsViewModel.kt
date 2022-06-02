63cba03ab045f9ced0722d1c14636be35ea68bca
mtbhiro <mtbhiro.developer@gmail.com>
2022-06-02 14:55:48 +0900

    init

---
+++ app/src/main/java/com/mtbhiro/bottom_navigation_flutter_sample/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.mtbhiro.bottom_navigation_flutter_sample.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
